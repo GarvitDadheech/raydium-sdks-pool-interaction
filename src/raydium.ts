6fa5de6a894c285f3d05a084738819737b28b67e
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2025-02-22 12:26:36 +0530

    mainnet init

--- src/raydium.ts
+++ src/raydium.ts
@@ -3,8 +3,8 @@ import { Connection, Keypair, clusterApiUrl } from '@solana/web3.js'
 import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
 import bs58 from 'bs58'
 
-export const owner: Keypair = Keypair.fromSecretKey(bs58.decode('<Your Wallet Private Key>'))
-export const connection = new Connection('<YOUR_RPC_URL>') 
+export const owner: Keypair = Keypair.fromSecretKey(bs58.decode('<YOUR_WALLET_SECRET_KEY>'))
+export const connection = new Connection('<YOUR_RPC_URL>') //<YOUR_RPC_URL>
 // export const connection = new Connection(clusterApiUrl('devnet')) //<YOUR_RPC_URL>
 export const txVersion = TxVersion.V0 // or TxVersion.LEGACY
 const cluster = 'mainnet' // 'mainnet' | 'devnet'
@@ -12,8 +12,8 @@ const cluster = 'mainnet' // 'mainnet' | 'devnet'
 let raydium: Raydium | undefined
 export const initSdk = async (params?: { loadToken?: boolean }) => {
   if (raydium) return raydium
-  // if (connection.rpcEndpoint === clusterApiUrl('devnet'))
-  //   console.warn('using free rpc node might cause unexpected error, strongly suggest uses paid rpc node')
+  if (connection.rpcEndpoint === clusterApiUrl('mainnet-beta'))
+    console.warn('using free rpc node might cause unexpected error, strongly suggest uses paid rpc node')
   console.log(`connect to rpc ${connection.rpcEndpoint} in ${cluster}`)
   raydium = await Raydium.load({
     owner,
